f9d90c77fa8f2c7e272c7e9feb64b9e151c5fb97
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-18 20:58:26 -0300

    Ch 1.6 Getting SwiftUi Layout Right Part 2: Spacers, Colors, safeArea & More!

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,28 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = ""
+    @State private var messageString = "When the Genius Bar needs help, they call you!"
     
     var body: some View {
-        VStack {
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-                .padding()
+        
+        
+       ZStack {
+            Rectangle()
+                .fill(
+                    Gradient(colors: [.white, .blue, .red])
+                )
             
-            HStack {
-                Button("Awesome") {
-                    // This is the action performed when the button is pressed
-                    messageString = "You Are Awesome!"
-                }
-                .buttonStyle(.borderedProminent)
+            VStack {
+                Text("You Have Skills!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(Color("Gold-BC"))
+                    .padding()
+                    .background(Color("Maroon-BC"))
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
                 
-                Button("Great") {
-                    // This is the action performed when the button is pressed
-                    messageString = "You Are Great!"
+                Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+    //                .border(.orange, width: 1)
+                    .padding()
+                
+                Spacer()
+                            
+                HStack {
+                    Button("Awesome") {
+                        // This is the action performed when the button is pressed
+                        messageString = "You Are Awesome!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Spacer()
+                    
+                    Button("Great") {
+                        // This is the action performed when the button is pressed
+                        messageString = "You Are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
                 }
-                .buttonStyle(.borderedProminent)
+    //            .border(.orange, width: 5)
+                .padding()
+                
                 
             }
             
