64059edc3cd35c8f3ea3eca50e42acbe822d77ff
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-18 13:25:05 -0300

    Ch 1.4 Button Challenges, Trailing Closures, HStacks

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,21 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "You Are Great!"
+    @State private var messageString = ""
     
     var body: some View {
         VStack {
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
-                .foregroundStyle(.mint)
+                .foregroundStyle(.red)
                 .padding()
             
-            Button("Press Me") {
-                // This is the action performed when the button is pressed
-                messageString = "You Are Awesome!"
+            HStack {
+                Button("Awesome") {
+                    // This is the action performed when the button is pressed
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Button("Great") {
+                    // This is the action performed when the button is pressed
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
+                
             }
-
+            
         }
     }
 }
