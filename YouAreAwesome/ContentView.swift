f38d9a051db98d779783020bffeb3eedb992cf2d
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-19 12:25:39 -0300

    Ch 1.13 Arrays in Swift

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var messageString = ""
+    @State private var imageString = ""
     @State private var isAwesome = false
     @State private var imageNumber = 0
+    @State private var messageNumber = 0
+    var messages = ["You Are Awesome!", "You Are Great!", "Fabolous? That's You!", "You Are Fantastic"]
+
     
     var body: some View {
         VStack {
-            Image("image\(imageNumber)")
+            Image(imageString)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
@@ -35,9 +39,10 @@ struct ContentView: View {
             Spacer()
             
             Button("Show Message") {
+                imageString = "image\(imageNumber)"
+                messageString = messages[messageNumber]
                 imageNumber = imageNumber >= 9 ? 0 : imageNumber + 1
-                
-                messageString = !isAwesome ? "You Are Awesome!" : "You Are Great!"
+                messageNumber = messageNumber >= messages.count - 1 ? 0 : messageNumber + 1
                 //TODO: DO SOMETHING
                 isAwesome.toggle()
             }
