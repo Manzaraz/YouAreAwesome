16c39a69a3da255cb65135c0b2c148142e2f2a11
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-18 22:28:36 -0300

    Ch 1.7 Getting SwiftUI Layout Right Part 3: Divider, GeometryRader & Group

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,65 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "When the Genius Bar needs help, they call you!"
+    @State private var messageString = ""
     
     var body: some View {
-        
-        
-       ZStack {
-            Rectangle()
-                .fill(
-                    Gradient(colors: [.white, .blue, .red])
-                )
+        VStack {
             
-            VStack {
-                Text("You Have Skills!")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .foregroundStyle(Color("Gold-BC"))
-                    .padding()
-                    .background(Color("Maroon-BC"))
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-                
-                Spacer()
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(.red)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-    //                .border(.orange, width: 1)
-                    .padding()
-                
-                Spacer()
-                            
-                HStack {
-                    Button("Awesome") {
-                        // This is the action performed when the button is pressed
-                        messageString = "You Are Awesome!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    Spacer()
-                    
-                    Button("Great") {
-                        // This is the action performed when the button is pressed
-                        messageString = "You Are Great!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                }
-    //            .border(.orange, width: 5)
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+            //                .border(.orange, width: 1)
                 .padding()
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    // This is the action performed when the button is pressed
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
                 
+                Spacer()
+                
+                Button("Great") {
+                    // This is the action performed when the button is pressed
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
                 
             }
-            
+            //            .border(.orange, width: 5)
+            .padding()
         }
     }
+    
 }
 
 #Preview {
