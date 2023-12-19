07a3b39851d91911bb14b6f317b3aeb767b6a36c
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-18 23:25:50 -0300

    Ch 1.8 SwiftUI Systme Images + SF Symbols

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -14,8 +14,22 @@ struct ContentView: View {
         VStack {
             
             Spacer()
+
+            Image(systemName: "speaker.wave.2", variableValue: 0.51)
+                .resizable()
+                .scaledToFit()
+                .symbolRenderingMode(.multicolor)
+                .padding()
+                .background(Color(hue: 0.521, saturation: 0.299, brightness: 0.958))
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(color: .gray, radius: 30, x: 20, y: 20)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(.teal, lineWidth: 5)
+                )
+                .padding()
             
-            Text(messageString)
+            Text("Namaste")
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .minimumScaleFactor(0.5)
@@ -23,8 +37,8 @@ struct ContentView: View {
                 .foregroundStyle(.red)
                 .frame(height: 150)
                 .frame(maxWidth: .infinity)
-            //                .border(.orange, width: 1)
                 .padding()
+
             
             Spacer()
             
@@ -44,7 +58,6 @@ struct ContentView: View {
                 .buttonStyle(.borderedProminent)
                 
             }
-            //            .border(.orange, width: 5)
             .padding()
         }
     }
